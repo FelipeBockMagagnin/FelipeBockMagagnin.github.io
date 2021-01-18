71c10655ed34f2cba1a0011edba46808f14eb613
Felpi <felipebmagagnin@gmail.com>
2021-01-18 08:15:19 -0300

    chore: improve projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import HomeButton from '../../components/homeButton'
+import bethehero from '../../images/print1.png'
+import './styles.css'
 
-export default function Projects(){
+export default function Projects() {
   return (
     <div className='flexDivCenter'>
       <HomeButton />
-      Projects
+
+
+      <a className='card withoutTextDecoration' href='https://github.com/FelipeBockMagagnin/BeTheHero' target='__blank' >
+        <img src={bethehero} className='cardImage' />
+
+        <div className='cardBody'>
+          BeTheHero
+
+          <div className='tagContainer'>
+            <div className='tag'>
+              Node.js
+            </div>
+
+            <div className='tag'>
+              React
+            </div>
+
+            <div className='tag'>
+              React Native
+            </div>
+
+            <div className='tag'>
+              Expo
+            </div>
+          </div>
+
+        </div>
+      </a>
     </div>
   );
 }
